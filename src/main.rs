604e6de3447903c39aa3c41a0791bed17301d042
ShantanuVichare <sheradgrr54@gmail.com>
2020-07-26 22:52:49 +0530

    Checking requests

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,33 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+// use serde_derive::Deserialize;
+use serde::{Serialize, Deserialize};
 
-async fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+// #[derive(Debug, Deserialize)]
+// struct Info {
+//     username: String,
+// }
+
+async fn index(req: HttpRequest) -> impl Responder {
+    println!("Request:\n{:?}", req);
+    // println!("Body:\n{:?}", body);
+    HttpResponse::Ok().body("At the root")
 }
 
-async fn index2() -> impl Responder {
-    HttpResponse::Ok().body("Hello world again!")
+async fn start_handler(req: HttpRequest) -> impl Responder {
+    println!("Request:\n{:?}", req);
+    HttpResponse::Ok().body("Hello!")
 }
+
+async fn move_handler(req: HttpRequest) -> impl Responder {
+    println!("Request:\n{:?}", req);
+    HttpResponse::Ok().body("Move!")
+}
+
+async fn end_handler(req: HttpRequest) -> impl Responder {
+    println!("Request:\n{:?}", req);
+    HttpResponse::Ok().body("Bye!")
+}
+
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
@@ -15,12 +36,18 @@ async fn main() -> std::io::Result<()> {
         .parse()
         .expect("PORT must be a number");
 
+    let ip = &std::env::var("IP")
+        .unwrap_or_else(|_| "127.0.0.1".to_string());
+
+    println!("Attempting to host at {}:{}", ip, port);
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(index))
-            .route("/again", web::get().to(index2))
+            .route("/start", web::get().to(start_handler))
+            .route("/move", web::post().to(move_handler))
+            .route("/end", web::post().to(end_handler))
     })
-    .bind(("0.0.0.0", port))
+    .bind((ip as &str, port))
     .expect(&format!("Can not bind to port {}",port))
     .run()
     .await
